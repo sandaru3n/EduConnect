87cd412e253c215d7d6f9405855ac101a63fa4f0
Sandaru Peiris <sandaru3n@gmail.com>
2025-05-05 23:42:45 +0530

    All Teachers  Manage feature added
    All Teachers  Manage feature added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,30 @@
 // backend/routes/adminRoutes.js
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { getAllTeachers, banTeacher,unbanTeacher,deleteTeacher,updateTeacher } = require('../controllers/adminController');
+const authMiddleware = require('../middleware/auth');
+
+// Get all teachers (admin role check moved to controller)
+router.get('/teachers', authMiddleware, getAllTeachers);
+
+// Ban a teacher by setting subscriptionStatus to inactive (admin role check moved to controller)
+router.put('/teachers/:teacherId/ban', authMiddleware, banTeacher);
+
+router.put('/teachers/:teacherId/unban', authMiddleware, unbanTeacher);
+
+// Delete a teacher (admin role check in controller)
+router.delete('/teachers/:teacherId', authMiddleware, deleteTeacher);
+
+// Update a teacher's details (admin role check in controller)
+router.put('/teachers/:teacherId', authMiddleware, updateTeacher);
+
 
 // Sample route for admin
 router.get("/", (req, res) => {
     res.send("Admin Route Working!");
 });
+
+
 
 module.exports = router;
